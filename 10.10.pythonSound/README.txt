501a5a497a75202532e5cebd45f0170371acb4e9
BancRoland <bancroland@gmail.com>
2023-01-30 10:18:40 +0100

    readme added

---
+++ 10.10.pythonSound/README.txt
@@ -0,0 +1,2 @@
+kell hozzá a pthon-sounddevice
+roland@roland-Latitude-3380:~/Desktop/Python/10.10.pythonSound$ python3 -m pip install sounddevice --user
---
+++ animation/run0.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+x = np.arange(0, 2*np.pi, 0.1)
+y = np.sin(x)
+
+fig, axes = plt.subplots(nrows=6)
+
+styles = ['r-', 'g-', 'y-', 'm-', 'k-', 'c-']
+def plot(ax, style):
+    return ax.plot(x, y, style, animated=True)[0]
+lines = [plot(ax, style) for ax, style in zip(axes, styles)]
+
+def animate(i):
+    for j, line in enumerate(lines, start=1):
+        line.set_ydata(np.sin(j*x + i/10.0))
+    return lines
+
+# We'd normally specify a reasonable "interval" here...
+ani = animation.FuncAnimation(fig, animate, range(1, 200), 
+                              interval=0, blit=True)
+plt.show()
