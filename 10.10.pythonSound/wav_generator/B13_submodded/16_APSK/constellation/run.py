5c6f81c9552c8f0afd0976810f52392eb8fee804
BancRoland <bancroland@gmail.com>
2023-03-03 13:54:27 +0100

    submod

---
+++ 10.10.pythonSound/wav_generator/B13_submodded/16_APSK/constellation/run.py
@@ -0,0 +1,110 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io.wavfile import write
+
+
+Nz=13      #kitöltőnullák
+sr=100      #samprate
+fs=44100    
+f=1000      #modFrq
+rep=1     #repeat
+QAM_N=4  # 4*4 es QAM
+
+
+# beadott v vektort R szeresére incrementálja
+def inc(v, R):
+    w=np.zeros(len(v)*R)+1j*np.zeros(len(v)*R)
+    print(len(v)*R)
+    for i in range(len(v)):
+        for j in range(R):
+            #print(i*R+j)
+            #w[i*R+j]=v[i]
+            w[i*R+j]=v[i]
+    return(w)
+
+#beadott v vektort a szor ismétli
+def repeat(v,a):
+    w=np.zeros(len(v)*a)+1j*np.zeros(len(v)*a)
+    for i in range(a):
+        w[(i*len(v)):((i+1)*len(v))]=np.array(v)
+    return(w)
+
+#beadott v vektort felkeveri f frekvncijú harmónikussal fs mintavétel mellett
+def upmix(v,f,fs):
+    P=np.arange(len(v))*f/fs*2*np.pi
+    mix=np.exp(1j*P)
+    w=v*mix
+    return(w)
+
+#beadott v vektor w decimálja
+def dec(v,a):
+    w=v[::a]
+    return(w)
+
+#beadott v vektort b/fs ujraminteveszi
+def resa(v,fs,b):
+    w=dec(inc(v,fs),b)
+    return(w)
+
+#beadott m vektort c kóddal moduálja
+def encode(m,c):
+    wlen=len(m)*len(c)
+    w=np.zeros(wlen)+1j*np.zeros(wlen)
+    for i in range(len(m)):
+        w[(i*len(c)):((i+1)*len(c))]=m[i]*c
+    return(w)
+
+
+
+
+# codeM0=np.array([-3-3j, -1-3j, 1-3j, 3-3j,  -3-1j, -1-1j, 1-1j, 3-1j,  -3+1j, -1+1j, 1+1j, 3+1j,  -3+3j, -1+3j, 1+3j, 3+3j])
+codeM0=np.zeros(QAM_N**2)+1j*np.zeros(QAM_N**2)
+for i in range(QAM_N):
+    for j in range(QAM_N):
+        codeM0[i*QAM_N+j]=i+1j*j
+codeM0=codeM0-((QAM_N-1)/2)*(1+1j)
+
+
+codeM=codeM0/np.max(np.abs(codeM0))     #Normált konstelláiós sorozat
+
+codeC=[1,1,1,1,1,0,0,1,1,0,1,0,1]   #ez a alap kód, ezt ne buzeráld
+codeB13=(np.array(codeC)+1j*np.zeros(len(codeC)))*2-1
+
+codeR=np.random.randint(QAM_N**2, size=1000)
+
+code10=encode(codeM[codeR],codeB13)
+
+
+plt.plot(np.real(codeM),np.imag(codeM),'o:')
+plt.title(f'bitrate: {sr} simbol/sec, sampFrq: {fs} Hz,\n codeLen: {len(codeC)}, zeros: {Nz}')
+plt.grid()
+plt.axis('square')
+plt.xlim(-1,1)
+plt.ylim(-1,1)
+plt.savefig('codeCONST.png')
+plt.show()
+
+# code11=np.concatenate([code10,np.zeros(Nz)])
+code2=repeat(code10,rep)
+code21=np.concatenate([np.zeros(Nz),code2])
+#code3=resa(code21,fs,sr)
+code3=inc(code21,int(fs/sr))
+code4=upmix(code3,f,fs)
+
+plt.plot(np.real(code10),'.-')
+plt.plot(np.imag(code10),'.-')
+plt.title(f'bitrate: {sr} simbol/sec, sampFrq: {fs} Hz,\n codeLen: {len(codeC)}, zeros: {Nz}')
+plt.grid()
+plt.savefig('codeTIME0.png')
+plt.show()
+
+plt.plot(np.real(code21),'.-')
+plt.plot(np.imag(code21),'.-')
+plt.title(f'bitrate: {sr} simbol/sec, sampFrq: {fs} Hz,\n codeLen: {len(codeC)}, zeros: {Nz}')
+plt.grid()
+plt.savefig('codeTIME.png')
+plt.show()
+
+
+scaled = np.int16(np.real(code4) * 32767)
+write('out.wav', fs, scaled)
--- 10.10.pythonSound/wav_generator/B13_submodded/QAM/run.py
+++ 10.10.pythonSound/wav_generator/B13_submodded/QAM/run.py
@@ -11,7 +11,7 @@ rep=10      #repeat
 
 
 def inc(v, R):
-    w=np.zeros(len(v)*R)
+    w=np.zeros(len(v)*R)+1j*np.zeros(len(v)*R)
     print(len(v)*R)
     for i in range(len(v)):
         for j in range(R):
@@ -21,7 +21,7 @@ def inc(v, R):
     return(w)
 
 def repeat(v,a):
-    w=np.zeros(len(v)*a)
+    w=np.zeros(len(v)*a)+1j*np.zeros(len(v)*a)
     for i in range(a):
         w[(i*len(v)):((i+1)*len(v))]=np.array(v)
     return(w)
@@ -40,21 +40,34 @@ def resa(v,fs,b):
     w=dec(inc(v,fs),b)
     return(w)
 
+def encode(m,c):
+    w=np.zeros(len(m)*len(c))+1j*np.zeros(len(m)*len(c))
+    for i in range(len(m)):
+        w[(i*len(c)):((i+1)*len(c))]=m[i]*c
+    return(w)
 
 
+codeM0=[0,1,2,3]
 
-code=[1,1,1,1,1,0,0,1,1,0,1,0,1]
-code1=np.array(code)*2-1
-code11=np.concatenate([code1,np.zeros(Nz)])
-code2=repeat(code11,rep)
+codeC=[1,1,1,1,1,0,0,1,1,0,1,0,1]   #ez a alap kód, ezt ne buzeráld
+
+codeM=np.exp(1j*np.array(codeM0)/4*2*np.pi)
+code1=(np.array(codeC)+1j*np.zeros(len(codeC)))*2-1
+code10=encode(codeM,code1)
+plt.plot(np.real(code10),'.-')
+plt.plot(np.imag(code10),'.-')
+plt.show()
+# code11=np.concatenate([code10,np.zeros(Nz)])
+code2=repeat(code10,rep)
 code21=np.concatenate([np.zeros(Nz),code2])
 #code3=resa(code21,fs,sr)
 code3=inc(code21,int(fs/sr))
 code4=upmix(code3,f,fs)
 
 
-plt.plot(code21,'.-')
-plt.title(f'bitrate: {sr} simbol/sec, sampFrq: {fs} Hz,\n codeLen: {len(code)}, zeros: {Nz}')
+plt.plot(np.real(code21),'.-')
+plt.plot(np.imag(code21),'.-')
+plt.title(f'bitrate: {sr} simbol/sec, sampFrq: {fs} Hz,\n codeLen: {len(codeC)}, zeros: {Nz}')
 plt.grid()
 plt.savefig('code.png')
 plt.show()
