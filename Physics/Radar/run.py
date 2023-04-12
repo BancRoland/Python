7686d741bfbadbfb8caf60ed267e1a6dd5897c7c
kuvik_PC <bancroland@gmail.com>
2023-04-12 18:12:32 +0200

    .

--- Physics/Radar/run.py
+++ Physics/Radar/run.py
@@ -20,6 +20,7 @@ parser.add_argument("-sr","--samprate", help="Samplerate of the signalflow [samp
 parser.add_argument("-Nc","--Ncode", help="code samples number [samples] ", nargs='?', type=int, required=True)
 parser.add_argument("-Nz","--Nzeros", help="zero samples number [samples] ", nargs='?', type=int, required=True)
 parser.add_argument("-dS","--dSamp", help="doppler samples number [line of samples] ", nargs='?', type=int, required=True)
+parser.add_argument("-dD","--dDec", help="doppler decimation value [samples] ", nargs='?', type=int, default=1)
 
 
 # parser.add_argument("-r","--radius", help="radius of the circle", nargs='?', type=float, const=1, default=7, required=True)
@@ -35,11 +36,12 @@ cFrq=args.cFrq # centerFrequency[Hz]
 sr=args.samprate     #sample rate [samp/sec]
 Ncode=args.Ncode   #correlation code samples []
 Nzeros=args.Nzeros #zeros for range []
-dSamp=args.dSamp   #dopplerSamples
+dSamp=args.dSamp    #dopplerSamples
+dDec=args.dDec      #doppler decimation value
 
 
 print("\n-------------Wave-------------")
-print(f'c= \t\t\033[1m{c} m/s\033[0m')
+print(f'c= \t\t\033[1m{c:,} m/s\033[0m'.replace(',', ' '))
 print(f'cFrq= \t\t\033[1m{cFrq/1e9} GHz\033[0m')
 print(f'lambda= \t\033[1m{c/cFrq} m\033[0m')
 print("\n-------------CODE-------------")
@@ -53,10 +55,13 @@ print(f'PRF= \t\t\033[1m{PRF/1000:.2f} kHz\033[0m')
 fillFactor=100*Ncode/fullLen
 print(f'FillFactor= \t\033[1m{fillFactor:.2f} %\033[0m')
 loadbar(fillFactor/2,100/2)
+print(f'doppler Samples= \t\033[1m{dSamp} \033[0m')
+print(f'doppler Decimation= \t\033[1m{dDec} \033[0m')
 print("\n-------------OPERATION-------------")
-print(f'Min Range= \t\033[1m{Ncode/sr*c/2/1000:.2f} km \033[0m')
-print(f'Max Range= \t\033[1m{fullLen/sr*c/2/1000:.2f} km\033[0m')
+print(f'Min Range= \t\033[1m{Ncode/sr*c/2:,} m \033[0m'.replace(',', ' '))
+print(f'Max Range= \t\033[1m{fullLen/sr*c/2:,} m \033[0m'.replace(',', ' '))
 print(f'Range Res= \t\033[1m{fullLen/sr*c/2/fullLen:.2f} m\033[0m')
-blindSpeed=PRF*c/cFrq
-print(f'Max velocity= \t\033[1m+-{blindSpeed/4:.2f} m/sec = {blindSpeed/4*3.6:.2f} km/h\033[0m')
-print(f'velocity res= \t\033[1m{blindSpeed/2/dSamp:.2f} m/sec = {blindSpeed/2/dSamp*3.6:.2f} km/h\033[0m\n')
+blindSpeed=PRF*c/cFrq/2
+print(f'Blind speed= \t\033[1m{blindSpeed/dDec:.2f} m/sec = {blindSpeed/dDec*3.6:.2f} km/h\033[0m')
+print(f'Max velocity= \t\033[1m+-{blindSpeed/2/dDec:.2f} m/sec = {blindSpeed/2/dDec*3.6:.2f} km/h\033[0m')
+print(f'velocity res= \t\033[1m{blindSpeed/dSamp:.2f} m/sec = {blindSpeed/dSamp*3.6:.2f} km/h\033[0m\n')
--- Physics/Radar/run.sh
+++ Physics/Radar/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 CFRQ=10e9 # centerFrequency[Hz]
-SR=100e6     #sample rate [samp/sec]
-NCODE=1024   #correlation code samples []
-NZEROS=6144 #zeros for range []
+SR=10e6     #sample rate [samp/sec]
+NCODE=16   #correlation code samples []
+NZEROS=128 #zeros for range []
 DSAMP=512   #dopplerSamples
+DDEC=16     #dopplerDecimation
 
 cat header.txt
 
-python3 run.py -c 3e8 -cFrq $CFRQ -sr $SR -Nc $NCODE -Nz $NZEROS -dS $DSAMP
+python3 run.py -c 3e8 -cFrq $CFRQ -sr $SR -Nc $NCODE -Nz $NZEROS -dS $DSAMP -dD $DDEC
