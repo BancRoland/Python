9c1768dceb0f7d12e11788f51ea65816b146aa61
BancRoland <bancroland@gmail.com>
2023-08-24 14:36:09 +0200

    .

--- random/run.py
+++ random/run.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 my_dpi=96
 
+print(hex(int(2**24*np.random.rand()))[2::])
+
 size=100
 r=np.random.rand(size, size)
 plt.figure(figsize=(size/my_dpi, size/my_dpi), dpi=my_dpi)
