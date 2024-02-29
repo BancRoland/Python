fee677f06ec9476d40c28592337f10bb7c580491
roland <bancroland@gmail.com>
2024-02-29 19:57:58 +0100

    .

--- read_write_files/read_dat/cui8/segmented/power_spectrum/segmented_analisis/read_dat_cui8.py
+++ read_write_files/read_dat/cui8/segmented/power_spectrum/segmented_analisis/read_dat_cui8.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-# import uhd
 import numpy as np
 from scipy import fftpack
 import matplotlib.pyplot as plt
@@ -12,6 +11,12 @@ import dsp
 
 
 os.makedirs("output", exist_ok = True) 
+os.makedirs("output/cpi", exist_ok = True)
+os.makedirs("output/cpi/raw", exist_ok = True)
+os.makedirs("output/cpi/filt", exist_ok = True) 
+os.makedirs("output/powspec", exist_ok = True) 
+os.makedirs("output/powspec/full_powspec", exist_ok = True) 
+os.makedirs("output/powspec/filt_powspec", exist_ok = True) 
 
 def power_fft(signal):
     return np.fft.fftshift(np.abs(np.fft.fft(signal/len(signal)))**2)
@@ -19,102 +24,116 @@ def power_fft(signal):
 def read_samples(file, LEN, offset=0):
     samples0    = np.fromfile(file, dtype=np.uint8, count=2*LEN, offset=2*LEN*offset)
     samples     = samples0[0::2]+1j*samples0[1::2]
-    samples     = samples-128*(1+1j)
+    samples     = samples-127*(1+1j)
     return samples
-
-def plt_plot(samples):
-    plt.figure()
-    # plt.plot(samples,'.-')
-    plt.plot(np.real(samples),  '.-',   color='C0',     alpha=1,    label="Real")
-    plt.plot(np.imag(samples),  '.-',   color='C1',     alpha=1,    label="Imag")
-    plt.plot(np.abs(samples),   '--',   color='grey',   alpha=0.5,  label="Abs")
-    plt.plot(-np.abs(samples),  '--',   color='grey',   alpha=0.5,  label="-Abs")
-    plt.legend()
-    plt.ylim([-128,128])
-    # plt.title(title)
-    plt.grid()
-    plt.show()
 
 
 parser = argparse.ArgumentParser()
-# parser.add_argument("file", help="file to read")
-parser.add_argument("-f","--file",      help="file to read from",   nargs='?', type=str, required=True)
-parser.add_argument("-l","--len",       help="length of segents",   nargs='?', type=int, required=True)
-parser.add_argument("-rf","--frq",      help="radio frequency",     nargs='?', type=int, required=True)
-parser.add_argument("-s","--samp_rate", help="sample rate",         nargs='?', type=int, required=True)
+parser.add_argument("-f","--file",      help="file to read from",           nargs='?', type=str, required=True)
+parser.add_argument("-l","--len",       help="length of segents",           nargs='?', type=int, required=True)
+parser.add_argument("-rf","--frq",      help="radio frequency",             nargs='?', type=int, required=True)
+parser.add_argument("-s","--samp_rate", help="sample rate",                 nargs='?', type=int, required=True)
+parser.add_argument("-sf","--sig_frq",  help="relevant signal frequency",   nargs='?', type=int, required=True)
 args = parser.parse_args()
 
-file=args.file
-LEN=args.len
-rf=args.frq
-sr=args.samp_rate
-FIR_LEN = 100
-fc = 1000
-f_sig=rf+50750
-# 101250750
-
-# with open("out.dat", mode="rb") as input:
-#     samples = input.read()
-average_power_spectrum = np.zeros(LEN, dtype="complex")
-FULL_average_power_spectrum = np.zeros(LEN, dtype="complex")
-
-samples = read_samples(file, LEN=LEN, offset=0)
+file        = args.file
+LEN         = args.len
+rf          = args.frq
+sr          = args.samp_rate
+signal_frq  = args.sig_frq
+FIR_LEN     = 100
+fc          = 1000
+f_sig       = rf + signal_frq
 
 idx=0
 log=[]
-while len(samples) == LEN:
 
-    t = (np.arange(len(samples))+idx*LEN)/sr
+f_raw   = (np.arange(LEN)/LEN-0.5)*sr+rf
+f       = (np.arange(LEN)/LEN-0.5)*sr+f_sig
+t       = (np.arange(LEN)+idx*LEN)/sr
 
-    FULL_power_spectrum = power_fft(samples)
 
-    samples = dsp.filter_channel(samples, rf, sr, FIR_LEN=FIR_LEN, fc=fc, f_sig=f_sig)
-    # dsp.complex_plot(samples)
-    # plt.ylim([-15,15])
-    # # plt.show()
-    # plt.savefig(f"output/iq_{idx:04d}.png")
-    # plt.close()
+average_filt_power_spectrum = np.zeros(LEN, dtype="complex")
+raw_average_power_spectrum = np.zeros(LEN, dtype="complex")
 
-    plt.figure(figsize=(15,10))
-    # plt.plot(samples,'.-')
+raw_samples = read_samples(file, LEN=LEN, offset=0)
+
+
+while len(raw_samples) == LEN:
+
+    raw_power_spectrum = power_fft(raw_samples)
+
+    filt_samples = dsp.filter_channel(raw_samples, rf, sr, FIR_LEN=FIR_LEN, fc=fc, f_sig=f_sig)
+    filt_power_spectrum = power_fft(filt_samples)
     
+    average_filt_power_spectrum += filt_power_spectrum
+    raw_average_power_spectrum += raw_power_spectrum
+
+    log.append(np.sum(np.abs(filt_power_spectrum)))
+    
+
+    # ---------------
+    # DISLAY filtered SIGNAL
+    # ---------------
+
+    plt.figure(figsize=(12,9))
     plt.subplot(2,1,1)
-    plt.plot(t,20*np.log10(np.abs(samples)),   '.-',   color='C0',   alpha=1,  label="Abs")
-    plt.legend()
-    plt.ylim([-40,20])
-    # plt.title(title)
-    plt.grid()
-    plt.ylabel("power [dB]")
+    plt.plot(t,np.real(filt_samples),".-", color='C0', alpha=1, label="Real")
+    plt.plot(t,np.imag(filt_samples),".-", color='C1', alpha=1, label="Imag")
+    plt.plot(t,np.abs(filt_samples),"--", color='gray', alpha=0.8, label="Abs")
+    plt.plot(t,-np.abs(filt_samples),"-", color='gray', alpha=0.8, label="-Abs")
+    plt.legend(loc='upper left')
+    plt.ylabel("signal vaule []")
     plt.xlabel("time [sec]")
-    plt.title(f"Signal amplitude values in logarithmic scale at t= {t[0]:.2f}-{t[-1]:.2f} seconds")
-    # plt.savefig(f"output/log_abs_{idx:04d}.png")
-    # plt.close()
-
-    power_spectrum = power_fft(samples)
-    average_power_spectrum += power_spectrum
-    FULL_average_power_spectrum += FULL_power_spectrum
-
-    f=(np.arange(len(power_spectrum))/len(power_spectrum)-0.5)*sr+f_sig
+    plt.grid()
+    plt.ylim([-128,127])
+    
     plt.subplot(2,1,2)
-    plt.plot(f,10*np.log10(power_spectrum),".-")
+    plt.plot(f,10*np.log10(filt_power_spectrum),".-")
     plt.grid()
     plt.ylabel("power [dB]")
-    plt.xlabel("frequency [MHz]")
-    plt.title(f"average power spectrum")
-    plt.ylim([-40,20])
+    plt.xlabel("frequency [Hz]")
+    plt.title(f"average power spectrum of filtered signal")
+    # plt.ylim([-40,20])
     plt.suptitle(f"file: {file}")
-    # plt.show()
-    plt.savefig(f"output/out_{idx:04d}.png")
+    plt.savefig(f"output/cpi/filt/out_{idx:04d}.png")
     plt.close()
 
-    idx=idx+1
-    print(f"{idx}")
 
-    samples = read_samples(file, LEN=LEN, offset=idx)
-    log.append(np.sum(np.abs(power_spectrum)))
+    # ---------------
+    # DISLAY RAW SIGNAL
+    # ---------------
+
+    plt.figure(figsize=(12,9))
+    plt.subplot(2,1,1)
+    # dsp.complex_plot(samples)
+    plt.plot(t, np.real(raw_samples),   ".-",   color='C0',     alpha=1,    label="Real")
+    plt.plot(t, np.imag(raw_samples),   ".-",   color='C1',     alpha=1,    label="Imag")
+    plt.plot(t, np.abs(raw_samples),    "--",   color='gray',   alpha=0.8,  label="Abs")
+    plt.plot(t, -np.abs(raw_samples),   "-",    color='gray',   alpha=0.8,  label="-Abs")
+    plt.legend(loc='upper left')
+    plt.ylabel("signal vaule []")
+    plt.xlabel("time [sec]")
+    plt.grid()
+    plt.ylim([-128,127])
+    
+    plt.subplot(2,1,2)
+    plt.plot(f_raw,10*np.log10(raw_power_spectrum),".-")
+    plt.grid()
+    plt.ylabel("power [dB]")
+    plt.xlabel("frequency [Hz]")
+    plt.title(f"average power spectrum")
+    # plt.ylim([-40,20])
+    plt.suptitle(f"file: {file}")
+    plt.savefig(f"output/cpi/raw/out_{idx:04d}.png")
+    plt.close()
+
+
+    # -------------
+    # PRINT OUT LOGGED POWERS OF RELEWANT SIGNAL
+    # -------------
 
     t2=(np.arange(len(log)))*LEN/sr
-    # plt.figure(figsize=(10,8))
     plt.plot(t2,10*np.log10(log), ".-", color='C0', alpha=1, label="Abs")
     plt.grid()
     plt.ylabel("power [dB]")
@@ -123,45 +142,48 @@ while len(samples) == LEN:
     # plt.ylim([-10,20])
     plt.savefig(f"output/log.png")
     plt.close()
-    
-
-average_power_spectrum/=idx
-
-f=(np.arange(len(average_power_spectrum))/len(average_power_spectrum)-0.5)*sr+f_sig
-plt.figure()
-plt.plot(f,10*np.log10(average_power_spectrum))
-plt.grid()
-plt.ylabel("power [dB]")
-plt.xlabel("frequency [MHz]")
-plt.title("average power spectrum")
-plt.savefig(f"output/awg_power_spec.png")
-plt.close()
 
 
-FULL_average_power_spectrum/=idx
+    # -------------
+    # AWERAGE POWERSPECTRUM OF RAW SIGNAL
+    # -------------
 
-f=(np.arange(len(FULL_average_power_spectrum))/len(FULL_average_power_spectrum)-0.5)*sr+rf
-plt.figure()
-plt.plot(f,10*np.log10(FULL_average_power_spectrum))
-plt.grid()
-plt.ylabel("power [dB]")
-plt.xlabel("frequency [MHz]")
-plt.title("average power spectrum")
-plt.savefig(f"output/full_awg_power_spec.png")
-plt.close()
-# plt.show()
+    toprint_RAW_average_power_spectrum = raw_average_power_spectrum/(idx+1)
+   
+    plt.figure()
+    plt.plot(f_raw,10*np.log10(toprint_RAW_average_power_spectrum))
+    plt.ylim([-40,40])
+    plt.grid()
+    plt.ylabel("power [dB]")
+    plt.xlabel("frequency [Hz]")
+    plt.title("average power spectrum")
+    plt.savefig(f"output/powspec/full_powspec/full_awg_power_spec_{idx}.png")
+    plt.close()
+   
+    # -------------
+    # AWERAGE POWERSPECTRUM OF FILTERED SIGNAL
+    # -------------
+
+    toprint_filt_power_spectrum = average_filt_power_spectrum/(idx+1)
+
+    plt.figure()
+    plt.plot(f,10*np.log10(toprint_filt_power_spectrum))
+    plt.ylim([-40,40])
+    plt.grid()
+    plt.ylabel("power [dB]")
+    plt.xlabel("frequency [Hz]")
+    plt.title("average power spectrum")
+    plt.savefig(f"output/powspec/filt_powspec/awg_power_spec_{idx}.png")
+    plt.close()
 
 
+    # -------------
+    # INCREMENT INDEX, AND READ FRESH SAMPLES
+    # -------------
 
-    # plt.xlabel('xcím')
-    # plt.ylabel('ycím')
-    # title=('frq= %.2f MHz \nsamprate= %.2f Msamp/sec \nDate= %s' %(center_freq/1E6,sample_rate/1E6,dstr));
-    # print(title)
-    # plt.title(title)
-    # #plt.legend(['sin(x)','cos(x)'])
-    # plt.legend()
-    # plt.grid()
-    # plt.savefig(dstr+'.png')
+    idx=idx+1
+    print(f"{idx}")
 
-    #plt.close()
+    raw_samples = read_samples(file, LEN=LEN, offset=idx)
+
 
--- read_write_files/read_dat/cui8/segmented/power_spectrum/segmented_analisis/run.sh
+++ read_write_files/read_dat/cui8/segmented/power_spectrum/segmented_analisis/run.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# 14:13:47    a drón 7 m magaasan eltávolodik a az antennától 
-# 14:16:00    köberepülés 7 magasan 50 m suarú pályán kb 20 km/h-val de inkább 4 m magasság
-# 14:18:41    körbeforgattuk az antennát
-
-# timestamp=141347    #eltavolodas
-timestamp=141600  # korbererpules
-# timestamp=141841  # korbeforg
-
 # mkdir meas_$timestamp
 
-LENGTH=32768
-FILE=/home/roland/Desktop/borgond_dielect_ant/data/out_24_02_27_$timestamp.cf32
-FRQ=101200000
-SAMP=300000
+# LENGTH=32768
+LENGTH=8192
+# FILE=/home/roland/Desktop/borgond_dielect_ant/data/out_24_02_27_$timestamp.cf32
+FILE=/home/roland/Desktop/Python/read_write_files/write_dat/cui8/sine/out.cui8
+FRQ=100000000
+SAMP=100000
+SIG_FRQ=25000
 
-python3 read_dat_cui8.py -l $LENGTH -f $FILE -rf $FRQ -s $SAMP
+python3 read_dat_cui8.py -l $LENGTH -f $FILE -rf $FRQ -s $SAMP -sf $SIG_FRQ
